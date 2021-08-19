afb40ac69b88259d5b7a85aab6f63f2e624694db
gforce4n6 <64875999+gforce4n6@users.noreply.github.com>
2021-08-18 21:38:12 -0400

    Updated contributors

--- scripts/version_info.py
+++ scripts/version_info.py
@@ -6,5 +6,6 @@ rleapp_version = '0.1'
 rleapp_contributors = [
     [ 'Alexis Brignoni', 'https://abrignoni.com', '@AlexisBrignoni', 'https://github.com/abrignoni'],
     [ 'Yogesh Khatri', 'https://swiftforensics.com', '@SwiftForensics', 'https://github.com/ydkhatri'],
+    [ 'Geraldine Blay', 'https://gforce4n6.blogspot.com', '@i_am_the_gia', 'https://github.com/gforce4n6'],
 
 ]
