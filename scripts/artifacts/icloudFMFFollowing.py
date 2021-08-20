9166dce1ba2cf49e11d9abbefb33f0c90db17551
gforce4n6 <64875999+gforce4n6@users.noreply.github.com>
2021-08-19 20:24:42 -0400

    iCloud Returns
    Additional iCloud returns and changing break for continue

--- scripts/artifacts/icloudFMFFollowing.py
+++ scripts/artifacts/icloudFMFFollowing.py
@@ -4,7 +4,7 @@ import xlrd
 from scripts.artifact_report import ArtifactHtmlReport
 from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows
 
-def get_icloudReturnsLogs(files_found, report_folder, seeker, wrap_text):
+def get_icloudFMFFollowing(files_found, report_folder, seeker, wrap_text):
     
     for file_found in files_found:
         file_found = str(file_found)
@@ -28,29 +28,30 @@ def get_icloudReturnsLogs(files_found, report_folder, seeker, wrap_text):
         wb = xlrd.open_workbook(loc)
         sheetnames = wb.sheet_names() 
         sheet = wb.sheet_by_index(0)
-        dsid = sheet.cell_value(2, 0)
+        #dsid = sheet.cell_value(2, 0)
         
         for i in range(sheet.nrows):
             for j in range(sheet.ncols):
-                if i == 6:
+                if i == 1:
                     dth.append(sheet.cell_value(i, j))
-                if i >= 7:
+                if i >= 2:
                     list.append(sheet.cell_value(i, j))
-            if i >= 7:
+            if i >= 2:
                 data_list.append(list) 
             list =[]
         
         
         if data_list:
-            description = f'Sheet name: {sheetnames[0]} - {dsid}'
-            report = ArtifactHtmlReport('iCloud - Logs')
-            report.start_artifact_report(report_folder, 'iCloud - Logs', description)
+            #description = f'Sheet name: {sheetnames[0]} - {dsid}'
+            description = f'Sheet name: {sheetnames[0]}'
+            report = ArtifactHtmlReport('iCloud - FMF Following')
+            report.start_artifact_report(report_folder, 'iCloud - FMF Following', description)
             report.add_script()
             data_headers = (dth)
             report.write_artifact_data_table(data_headers, data_list, file_found)
             report.end_artifact_report()
             
         else:
-            logfunc('No iCloud - No Log data available')
+            logfunc('No iCloud - FMF Following data available')
             
-        
+    
