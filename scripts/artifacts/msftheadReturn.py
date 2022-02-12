3a1b95296ef43fb9a28a5ce765e8cf16f13f603d
abrignoni <abrignoni@gmail.com>
2022-02-11 20:48:48 -0500

    Update msftheadReturn.py
    Added 3 received by headers each in its own column for the small csv report format.

--- scripts/artifacts/msftheadReturn.py
+++ scripts/artifacts/msftheadReturn.py
@@ -9,6 +9,10 @@ from email.parser import HeaderParser
 from scripts.artifact_report import ArtifactHtmlReport
 from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, media_to_html
 
+def recdetail(val):
+    cleandata = val.split('by')[0].split('from')[1].replace('\n', '').strip()
+    return cleandata
+
 def get_msftheadReturn(files_found, report_folder, seeker, wrap_text):
 
     data_list = []
@@ -33,7 +37,7 @@ def get_msftheadReturn(files_found, report_folder, seeker, wrap_text):
         writer = csv.writer(f)
         writerg = csv.writer(g)
         headers = ['Timestamp', 'X Originating IP', 'From', 'To', 'Received', 'Path']
-        headersg = ['Timestamp', 'X Originating IP', 'From', 'To', 'Path']
+        headersg = ['Timestamp', 'X Originating IP', 'From', 'To', 'Received Last', 'Received Middle', 'Received First' 'Path']
         writer.writerow(headers)
         writerg.writerow(headersg)
         
@@ -50,6 +54,8 @@ def get_msftheadReturn(files_found, report_folder, seeker, wrap_text):
             headers = parser.parsestr(msg.as_string())
             
             fecha = orip = to = de = aggregator = fortsv = ''
+            receivedlast = receivedmiddle = receivedfirst = ''
+            count = 0
             
             for key, value in headers.items():
                 if key == 'Date':
@@ -62,12 +68,17 @@ def get_msftheadReturn(files_found, report_folder, seeker, wrap_text):
                     de = value
                 if key == 'Received':
                     aggregator = aggregator + value
-
-            
+                    if count == 0:
+                        receivedlast = recdetail(value)
+                    elif count == 1:
+                        receivedmiddle = recdetail(value)
+                    elif count == 2:
+                        receivedfirst = recdetail(value)
+                    count = count + 1
             
             data_list.append((fecha, orip, de, to, file_found))
             data = [fecha, orip, de, to, aggregator, file_found]
-            datag = [fecha, orip, de, to, file_found]
+            datag = [fecha, orip, de, to, receivedlast, receivedmiddle, receivedfirst, file_found]
             writer.writerow(data)
             writerg.writerow(datag)
             cursor.execute("INSERT INTO data VALUES(?,?,?,?,?,?)", (fecha, orip, de, to, aggregator, file_found))
