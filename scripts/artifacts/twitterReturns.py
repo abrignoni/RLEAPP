b9351928c00e76625a6d68ad20f495ad536dabd4
Alexis Bringoni <abrignoni@gmail.com>
2022-02-18 22:02:12 -0500

    Twitter update
    Added Devices.txt parsing.  Fixed source path for twitter artifacts.

--- scripts/artifacts/twitterReturns.py
+++ scripts/artifacts/twitterReturns.py
@@ -20,6 +20,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
     data_list_saved_searches = []
     data_list_screen_name_changes = []
     data_list_tweets = []
+    data_list_devices = []
     
     for file_found in files_found:
         file_found = str(file_found)
@@ -27,6 +28,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
         filename = os.path.basename(file_found)
         
         if filename.endswith('-dms.txt'):
+            filenamedms = file_found
             with open(file_found, 'r', encoding='utf-8') as f:
                 lines = f.readlines()
             count = 0    
@@ -64,6 +66,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
                     
                 
         if filename.endswith('-groupdm.txt'):
+            filenamegroupdm = file_found
             with open(file_found, 'r', encoding='utf-8') as f:
                 lines = f.readlines()
             count = 0    
@@ -114,6 +117,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
                     data_list_groupdm.append((timestamp, convoid, sid, rid, text, thumbs))
 
         if filename.endswith('-account.txt'):
+            filenameaccount = file_found
             with open(file_found, 'r', encoding='utf-8') as f:
                 lines = f.readlines()
             count = 0
@@ -151,6 +155,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
                     account_id = created_at = updated_at = email = created_via = screen_name = creation_ip = time_zone = ''
         
         if filename.endswith('-followers.txt'):
+            filenamefollowers = file_found
             with open(file_found, 'r', encoding='utf-8') as f:
                 lines = f.readlines()
             count = 0
@@ -167,6 +172,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
                         data_list_followers.append((line.strip(),))
     
         if filename.endswith('-following.txt'):
+            filenamefollowing = file_found
             with open(file_found, 'r', encoding='utf-8') as f:
                 lines = f.readlines()
             count = 0
@@ -183,6 +189,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
                         data_list_following.append((line.strip(),))
         
         if filename.endswith('-lists_created.txt'):
+            filenamelistscreated = file_found
             with open(file_found, 'r', encoding='utf-8') as f:
                 lines = f.readlines()
             count = 0
@@ -199,6 +206,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
                         data_list_lists_created.append((line.strip(),))
     
         if filename.endswith('-profile-description.txt'):
+            filenameprofiledesc = file_found
             with open(file_found, 'r', encoding='utf-8') as f:
                 lines = f.readlines()
             count = 0    
@@ -219,6 +227,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
                         data_list_profile_description.append((bio, website))
     
         if filename.endswith('-saved-searches.txt'):
+            filenamesavedsearch = file_found
             with open(file_found, 'r', encoding='utf-8') as f:
                 lines = f.readlines()
             count = 0    
@@ -243,6 +252,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
                         data_list_saved_searches.append((ids, query, name ))
         
         if filename.endswith('-screen-name-changes.txt'):
+            filenamescreenname = file_found
             with open(file_found, 'r', encoding='utf-8') as f:
                 lines = f.readlines()
             count = 0    
@@ -260,8 +270,46 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
                         nameto = (line.split('->')[1].split(' ')[1])
                         timechange = (line.split('->')[1].split('(')[1].split('+')[0].strip())
                         data_list_screen_name_changes.append((timechange, namefrom, nameto))
+                        
+        if filename.endswith('-devices.txt'):
+            filenamedevices = file_found
+            with open(file_found, 'r', encoding='utf-8') as f:
+                lines = f.readlines()
+            count = 0
+            createdat = updatedat = typed = ipxcarry = phonen = ''
+            for line in lines:
+                count += 1
+                
+                if count < 2:
+                    pass
+                    
+                if 'created_at:' in line:
+                    dated = (line.split(' '))
+                    createdat = f'{dated[1]} {dated[2]} {dated[3]}'
+                    
+                if 'updated_at:' in line:
+                    dated = (line.split(' '))
+                    updatedat = f'{dated[1]} {dated[2]} {dated[3]}'
+                    
+                if 'type:' in line:
+                    typed = (line.split(' '))
+                    typed = typed[1]
+                
+                if 'ipx_carrier_name:' in line:
+                    ipxcarry = (line.split(' '))
+                    ipxcarry = ipxcarry[1]
+                    
+                if 'phone_numer:' in line:
+                    phonen = (line.split(' '))
+                    phonen  = phonen[1]    
+                    
+                    if createdat == '' and updatedat == '' and typed == '' and ipxcarry == '' and phonen == '':
+                        pass
+                    else:
+                        data_list_devices.append((createdat, updatedat, typed, ipxcarry, phonen ))
         
         if filename.endswith('-tweets.txt'):
+            filenametweets = file_found
             aggregator = ' '
             dateds = timetochange = datetime_object = timestampeds = rtid = rttext = rtsource = rtcoor = ''
             with open(file_found, 'r', encoding='utf-8') as f:
@@ -316,7 +364,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
         report.start_artifact_report(report_folder, 'Twitter Returns - Tweets')
         report.add_script()
         data_headers = ('Timestamp','ID','Text','Media','Source','Coordinates','RT Timestamp','RT ID','RT Text','RT Source','RT Coordinates')
-        report.write_artifact_data_table(data_headers, data_list_tweets, file_found, html_no_escape=['Media'])
+        report.write_artifact_data_table(data_headers, data_list_tweets, filenametweets, html_no_escape=['Media'])
         report.end_artifact_report()
         
         tsvname = f'Twitter Returns - Tweets'
@@ -330,7 +378,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
         report.start_artifact_report(report_folder, 'Twitter Returns - Screen Name Changes')
         report.add_script()
         data_headers = ('Timestamp','Name From','Name To')
-        report.write_artifact_data_table(data_headers, data_list_screen_name_changes, file_found)
+        report.write_artifact_data_table(data_headers, data_list_screen_name_changes, filenamescreenname)
         report.end_artifact_report()
         
         tsvname = f'Twitter Returns - Screen Name Changes'
@@ -344,7 +392,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
         report.start_artifact_report(report_folder, 'Twitter Returns - Saved Searches')
         report.add_script()
         data_headers = ('ID','Query','Name')
-        report.write_artifact_data_table(data_headers, data_list_saved_searches, file_found)
+        report.write_artifact_data_table(data_headers, data_list_saved_searches, filenamesavedsearch)
         report.end_artifact_report()
         
         tsvname = f'Twitter Returns - Saved Searches'
@@ -358,7 +406,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
         report.start_artifact_report(report_folder, 'Twitter Returns - Profile Description')
         report.add_script()
         data_headers = ('Bio','Website')
-        report.write_artifact_data_table(data_headers, data_list_profile_description, file_found)
+        report.write_artifact_data_table(data_headers, data_list_profile_description, filenameprofiledesc )
         report.end_artifact_report()
         
         tsvname = f'Twitter Returns - Profile Description'
@@ -372,21 +420,21 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
         report.start_artifact_report(report_folder, 'Twitter Returns - Lists Created')
         report.add_script()
         data_headers = ('List',)
-        report.write_artifact_data_table(data_headers, data_list_lists_created, file_found)
+        report.write_artifact_data_table(data_headers, data_list_lists_created, filenamelistscreated)
         report.end_artifact_report()
         
         tsvname = f'Twitter Returns - Lists Created'
         tsv(report_folder, data_list_lists_created, data_headers, tsvname)
         
     else:
-        logfunc('No Twitter Returns - Following data available')
+        logfunc('No Twitter Returns - Lists Created data available')
         
     if data_list_following:
         report = ArtifactHtmlReport('Twitter Returns - Following')
         report.start_artifact_report(report_folder, 'Twitter Returns - Following')
         report.add_script()
         data_headers = ('Following',)
-        report.write_artifact_data_table(data_headers, data_list_following, file_found)
+        report.write_artifact_data_table(data_headers, data_list_following, filenamefollowing)
         report.end_artifact_report()
         
         tsvname = f'Twitter Returns - Following'
@@ -400,7 +448,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
         report.start_artifact_report(report_folder, 'Twitter Returns - Followers')
         report.add_script()
         data_headers = ('Followers',)
-        report.write_artifact_data_table(data_headers, data_list_followers, file_found)
+        report.write_artifact_data_table(data_headers, data_list_followers, filenamefollowers)
         report.end_artifact_report()
         
         tsvname = f'Twitter Returns - Followers'
@@ -415,7 +463,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
         report.start_artifact_report(report_folder, 'Twitter Returns - Account Data')
         report.add_script()
         data_headers = ('Created At', 'Screen Name', 'Account ID', 'Updated At', 'Email', 'Creation IP', 'Created Via', 'Time Zone')
-        report.write_artifact_data_table(data_headers, data_list_account, file_found)
+        report.write_artifact_data_table(data_headers, data_list_account, filenameaccount )
         report.end_artifact_report()
         
         tsvname = f'Twitter Returns - Account Data'
@@ -432,7 +480,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
         report.start_artifact_report(report_folder, 'Twitter Returns - Direct Messages')
         report.add_script()
         data_headers = ('Timestamp', 'Conversation ID', 'Sender ID', 'Recipient ID', 'Message', 'Media')
-        report.write_artifact_data_table(data_headers, data_list_dms, file_found, html_no_escape=['Media'])
+        report.write_artifact_data_table(data_headers, data_list_dms, filenamedms, html_no_escape=['Media'])
         report.end_artifact_report()
         
         tsvname = f'Twitter Returns - Direct Messages'
@@ -448,7 +496,7 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
         report.start_artifact_report(report_folder, 'Twitter Returns - Group Messages')
         report.add_script()
         data_headers = ('Timestamp', 'Conversation ID', 'Sender ID', 'Recipient ID', 'Message', 'Media')
-        report.write_artifact_data_table(data_headers, data_list_groupdm, file_found, html_no_escape=['Media'])
+        report.write_artifact_data_table(data_headers, data_list_groupdm, filenamegroupdm, html_no_escape=['Media'])
         report.end_artifact_report()
         
         tsvname = f'Twitter Returns - Group Messages'
@@ -459,4 +507,20 @@ def get_twitterReturns(files_found, report_folder, seeker, wrap_text):
     else:
         logfunc('No Twitter Returns - Group Messages data available')
         
+    
+    if data_list_devices:
+        report = ArtifactHtmlReport('Twitter Returns - Devices')
+        report.start_artifact_report(report_folder, 'Twitter Returns - Devices')
+        report.add_script()
+        data_headers = ('Created At', 'Updated At', 'Type', 'IPX Carrier Name', 'Phone Number')
+        report.write_artifact_data_table(data_headers, data_list_devices, filenamedevices)
+        report.end_artifact_report()
+        
+        tsvname = f'Twitter Returns - Devices'
+        tsv(report_folder, data_list_devices, data_headers, tsvname)
+        
+        tlactivity = f'Twitter Returns - Devices'
+        timeline(report_folder, tlactivity, data_list_devices, data_headers)
+    else:
+        logfunc('No Twitter Returns - Devices')
         
