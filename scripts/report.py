0f218e2dbd8f1d1ccf82499bdb24f48e43014e86
abrignoni <abrignoni@gmail.com>
2022-03-16 14:57:07 -0400

    Email used to Airdrop from Sysdiagnose in Apple Devices
    * Identifies the email of the sender of Airdrop files by using user provided suspected email addresses for hash matching.
    * Place suspected email addresses in the scripts/emails/emails.txt file. One email address per line.
    * Source data will be a subset of the exported ystem_logs.logarchive from a sysdiagnose log extraction.
    * Using a MacOS computer open terminal and execute the following: log show ./system_logs.logarchive --style ndjson --predicate 'category = "AirDrop"' > airdrop.ndjson
    * Open RLEAPP. Using the Browse folder button select the directory in your system that contains the airdrop.ndjson file. Select the ouput directory path. Run RLEAPP

--- scripts/report.py
+++ scripts/report.py
@@ -71,6 +71,7 @@ def get_icon_name(category, artifact):
     elif category == 'GOOGLE RETURNS MBOXES':            icon = 'mail'
     elif category == 'MICROSOFT RETURNS':            icon = 'target'
     elif category == 'AIRDROP NUMBERS':            icon = 'smartphone'
+    elif category == 'AIRDROP EMAILS':            icon = 'send'
     elif category == 'TWITTER RETURNS':            icon = 'twitter'
     elif category == 'DISCORD RETURNS':            icon = 'message-square'
     elif category == 'TIKTOK RETURNS':            icon = 'film'
