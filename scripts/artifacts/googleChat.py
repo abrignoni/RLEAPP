77ff39410c816b13ab4248b82357f95f567e4dc4
abrignoni <abrignoni@gmail.com>
2023-03-16 19:29:17 -0400

    Update googleChat.py
    Added images to report and fixed multiple bugs.

--- scripts/artifacts/googleChat.py
+++ scripts/artifacts/googleChat.py
@@ -9,8 +9,9 @@ import datetime
 import json
 import os
 
+from pathlib import Path
 from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows
+from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, media_to_html
 
 def get_googleChat(files_found, report_folder, seeker, wrap_text):
     
@@ -31,7 +32,7 @@ def get_googleChat(files_found, report_folder, seeker, wrap_text):
     
     for file_found in files_found:
         file_found = str(file_found)
-        print(file_found)
+        
         if not os.path.basename(file_found) == 'messages.json': # skip -journal and other files
             continue
 
@@ -39,15 +40,19 @@ def get_googleChat(files_found, report_folder, seeker, wrap_text):
             data = json.loads(f.read())
         
         group_info_file = os.path.dirname(file_found) + os.path.sep + 'group_info.json'
-        with open(group_info_file, encoding = 'utf-8', mode = 'r') as fg:
-            group_data = json.loads(fg.read())
-        
-        members = []
-        for member in group_data["members"]:
-            members.append(member.get('name', '') + ' - ' + member.get('email',''))
+        if Path(group_info_file).is_file():
+            with open(group_info_file, encoding = 'utf-8', mode = 'r') as fg:
+                group_data = json.loads(fg.read())
             
-        members_html = '<br>'.join(members)
-        members_tsv = '\n'.join(members)
+            members = []
+            for member in group_data["members"]:
+                members.append(member.get('name', '') + ' - ' + member.get('email',''))
+                
+            members_html = '<br>'.join(members)
+            members_tsv = '\n'.join(members)
+        else:
+            members_html = ''
+            members_tsv = ''
         
         for chat_message in data['messages']:
             month = ''
@@ -103,17 +108,16 @@ def get_googleChat(files_found, report_folder, seeker, wrap_text):
             message_text_tsv = message_text_tsv.replace(u'\xa0',u' ')    
             
             if 'attached_files' in chat_message:
-                attachments = chat_message['attached_files'][0].get('export_name','')
+                if len(chat_message['attached_files']) == 0:
+                    attachments = '' 
+                else:
+                    attachments = chat_message['attached_files'][0].get('export_name','')
+                    attachments = media_to_html(attachments, files_found, report_folder)
             else: 
-                attachments = '' 
+                attachments = ''
             
-            if 'attached_files' in chat_message:
-                attachments = chat_message['attached_files'][0].get('export_name','')
-            else: 
-                attachments = ''   
-               
+            
             data_list.append((datetime_stamp,sender_name,sender_email,sender_user_type,members_html,message_text,attachments))
-            
             data_list_tsv.append((datetime_stamp,sender_name,sender_email,sender_user_type,members_tsv,message_text_tsv,attachments,))
 
     num_entries = len(data_list)
@@ -121,9 +125,9 @@ def get_googleChat(files_found, report_folder, seeker, wrap_text):
         report = ArtifactHtmlReport('Google Chat - Messages')
         report.start_artifact_report(report_folder, 'Google Chat - Messages')
         report.add_script()
-        data_headers = ('Created Timestamp','Sender Name','Sender Email','Sender Type','Group Members','Message','Attachment Name')
+        data_headers = ('Created Timestamp','Sender Name','Sender Email','Sender Type','Group Members','Message','Attachment')
 
-        report.write_artifact_data_table(data_headers, data_list, file_found, html_no_escape=['Group Members'])
+        report.write_artifact_data_table(data_headers, data_list, file_found, html_no_escape=['Group Members','Attachment'])
         report.end_artifact_report()
         
         tsvname = f'Google Chat - Messages'
@@ -137,6 +141,6 @@ def get_googleChat(files_found, report_folder, seeker, wrap_text):
 __artifacts__ = {
         "googleChat": (
             "Google Takeout Archive",
-            ('*/Google Chat/Groups/*/messages.json', '*/Google Chat/Groups/*/group_info.json'),
+            ('*/Google Chat/Groups/*/**'),
             get_googleChat)
 }
