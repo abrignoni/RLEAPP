4f25db2d63e770fb77d0abb6e6d675c531590cd2
Brigs <abrignoni@gmail.com>
2024-04-05 17:12:22 -0400

    Discord pull data from URLs
    Discord returns pull data from URLs

--- scripts/artifacts/discordReturnsdms.py
+++ scripts/artifacts/discordReturnsdms.py
@@ -15,67 +15,68 @@ def get_discordReturnsdms(files_found, report_folder, seeker, wrap_text, time_of
         filename = os.path.basename(file_found)
         csvname = filename
         
-        if file_found.endswith('.csv'):
-            data_list_dm =[]
-            with open(file_found, 'r', errors='backslashreplace') as f:
-                for line in f:
-                    delimited = csv.reader(f, delimiter=',')
-                    for item in delimited:
-                        timestamp = item[3]
-                        username = item[4]
-                        contents = item[5]
-                        media = item[6]
-                        id = item[0]
-                        channelid = item[1]
-                        authorid = item[2]
-                        
-                        if media == '':
-                            agregator = ' '
-                        else:
-                            if '\n' in media:
-                                media = media.split('\n')
-                                agregator = '<table>'
-                                counter = 0
-                                for x in media:
-                                    if counter == 0:
-                                        agregator = agregator + ('<tr>')
-                                    media = x.split('/')
+        if not file_found.startswith('._'):
+            if file_found.endswith('.csv'):
+                data_list_dm =[]
+                with open(file_found, 'r', errors='backslashreplace') as f:
+                    for line in f:
+                        delimited = csv.reader(f, delimiter=',')
+                        for item in delimited:
+                            timestamp = item[3]
+                            username = item[4]
+                            contents = item[5]
+                            media = item[6]
+                            id = item[0]
+                            channelid = item[1]
+                            authorid = item[2]
+                            
+                            if media == '':
+                                agregator = ' '
+                            else:
+                                if '\n' in media:
+                                    media = media.split('\n')
+                                    agregator = '<table>'
+                                    counter = 0
+                                    for x in media:
+                                        if counter == 0:
+                                            agregator = agregator + ('<tr>')
+                                        media = x.split('/')
+                                        originalfilename = media[-1]
+                                        attachmentidentifier = f"{media[-2]}"
+                                        thumb = media_to_html(attachmentidentifier, files_found, report_folder)
+                                        counter = counter + 1
+                                        agregator = agregator + f'<td>{thumb}</td>'
+                                        #hacer uno que no tenga html
+                                        if counter == 2:
+                                            counter = 0
+                                            agregator = agregator + ('</tr>')
+                                    if counter == 1:
+                                        agregator = agregator + ('</tr>')
+                                    agregator = agregator + ('</table><br>')
+                                else:
+                                    media = media.split('/')
                                     originalfilename = media[-1]
                                     attachmentidentifier = f"{media[-2]}"
-                                    thumb = media_to_html(attachmentidentifier, files_found, report_folder)
-                                    counter = counter + 1
-                                    agregator = agregator + f'<td>{thumb}</td>'
-                                    #hacer uno que no tenga html
-                                    if counter == 2:
-                                        counter = 0
-                                        agregator = agregator + ('</tr>')
-                                if counter == 1:
-                                    agregator = agregator + ('</tr>')
-                                agregator = agregator + ('</table><br>')
-                            else:
-                                media = media.split('/')
-                                originalfilename = media[-1]
-                                attachmentidentifier = f"{media[-2]}"
-                                agregator = media_to_html(attachmentidentifier, files_found, report_folder)
-                        
-                        data_list_dm.append((timestamp,username,contents,agregator,id,channelid,authorid))
-                        
+                                    agregator = media_to_html(attachmentidentifier, files_found, report_folder)
+                            
+                            data_list_dm.append((timestamp,username,contents,agregator,id,channelid,authorid))
+                            
         
-            if data_list_dm:
-                report = ArtifactHtmlReport(f'Discord - Direct Messages ')
-                report.start_artifact_report(report_folder, f'Discord - Direct Messages - {csvname}')
-                report.add_script()
-                data_headers = ('Timestamp','Username','Contents','Media','ID','Channel ID','Author ID')
-                report.write_artifact_data_table(data_headers, data_list_dm, file_found, html_no_escape=['Media'])
-                report.end_artifact_report()
-                
-                tsvname = f'Discord - Direct Messages - {csvname}'
-                tsv(report_folder, data_headers, data_list_dm, tsvname)
-                
-                tlactivity = f'Discord -Direct Messages - {csvname}'
-                timeline(report_folder, tlactivity, data_list_dm, data_headers)
-            else:
-                logfunc(f'Discord - Direct Messages - {csvname}')
+                if data_list_dm:
+                    report = ArtifactHtmlReport(f'Discord - Direct Messages ')
+                    report.start_artifact_report(report_folder, f'Discord - Direct Messages - {csvname}')
+                    report.add_script()
+                    data_headers = ('Timestamp','Username','Contents','Media','ID','Channel ID','Author ID')
+                    report.write_artifact_data_table(data_headers, data_list_dm, file_found, html_no_escape=['Media'])
+                    report.end_artifact_report()
+                    
+                    tsvname = f'Discord - Direct Messages - {csvname}'
+                    tsv(report_folder, data_headers, data_list_dm, tsvname)
+                    
+                    tlactivity = f'Discord -Direct Messages - {csvname}'
+                    timeline(report_folder, tlactivity, data_list_dm, data_headers)
+                else:
+                    logfunc(f'Discord - Direct Messages - {csvname}')
                 
 __artifacts__ = {
         "discordReturnsdms": (
